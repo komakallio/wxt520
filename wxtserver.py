c078cabb27bf2aeaad182b14a1d2462e3d7e5095
Jari Saukkonen <jari.saukkonen@ambientia.fi>
2017-05-31 21:59:57 +0300

    Make wxt520 class stand-alone, implement simple relay server

---
+++ wxtserver.py
@@ -0,0 +1,13 @@
+import wxt520
+import requests
+
+if __name__ == '__main__':
+    wxt = wxt520.WXT520('/dev/ttyUSB1', address='0', timeout=10)
+
+    while 1:
+        line = wxt.readline()
+        if line:
+            data = WXT520.message_to_dict(line)
+            r = requests.post('http://localhost:9001/api/', json=data)
+            if r.status_code != 200:
+                print 'Received error %d' % r.status_code
